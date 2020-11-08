120baa2dc43aa747994f4cbf73438032d5df616b
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2020-11-09 03:18:52 +0800

    Dasar JavaScript

---
+++ pertemuan_23/index.js
@@ -0,0 +1,49 @@
+//variabel
+
+//let & const
+// let umur = 25;
+// let nilai = 95.25;
+// let sudahMenikah = false; //bolean
+// let nama = "iji"; //string
+// console.log(umur);
+// console.log(nama);
+
+// let alamat;
+// console.log(alamat); //undefined
+
+// const tahunLahir = 2001;
+// console.log(tahunLahir);
+      
+//Operator
+
+//Aritmatic Operator(+,-,*,:)
+
+// let angka = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let namaDepan = "Virjin";
+// let namaBelakang = "Husain";
+// console.log("Nama Saya" + namaDepan + " "+ namaBelakang + ". Umur saya adalah" + umur + "Tahun"
+// );
+
+//Operator Perbandingan
+
+// let angka1 = 10;
+// let angka2 = "10";
+ 
+// console.log(angka1 === angka2);
+
+//Ternary Operator
+// let angka = 5;
+// let hasil = angka < 0 ? "Negatif" : "Positif";
+//     console.log(hasil);
+
+// //Operator logika
+
+let kondisi1 = false;
+let kondisi2 = true;
+console.log(kondisi1&&kondisi2);
+
+console.log(!kondisi1);
+
